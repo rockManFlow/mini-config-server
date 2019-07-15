28dc5a41271e4437486af51d1d668c3b6d4c282a
caoqingyuan <1670903022@qq.com>
2019-07-15 19:30:57 +0800

    配置中心服务端首次提交

---
+++ src/main/java/com/rock/config/ConfigApplication.java
@@ -0,0 +1,15 @@
+package com.rock.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+//启动一个服务注册中心提供给其他应用进行对话
+//@EnableEurekaServer
+@SpringBootApplication(scanBasePackages = {"com.rock.config"})
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class,args);
+    }
+}
