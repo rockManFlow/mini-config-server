46af505fd92f6445b3bbfaf63d8a9f0e62ac514a
caoqingyuan <1670903022@qq.com>
2019-07-22 19:19:50 +0800

    提交

--- src/main/java/com/rock/config/ConfigApplication.java
+++ src/main/java/com/rock/config/ConfigApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-//启动一个服务注册中心提供给其他应用进行对话
+
 @SpringBootApplication(scanBasePackages = {"com.rock.config"})
+//启动一个配置中心提供给其他应用进行对话
 @EnableConfigServer
 public class ConfigApplication {
     public static void main(String[] args) {
